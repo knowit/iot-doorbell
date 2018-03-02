b40c762feabfc31214a019d4b115df4e800c7baa
Frode Ånonsen <frode@anonsen.org>
2018-03-02 14:54:42 +0100

    Better ring message

--- slack-client/doorbot.js
+++ slack-client/doorbot.js
@@ -12,7 +12,7 @@ client.on('message', (topic, message) => {
   if(topic === settings.mqttTopic) {
     console.log('on topic ' + settings.mqttTopic)
     console.log('Message: ' + message.toString())
-    webhook.send('Det ringer på! :door:', function(err, res) {
+    webhook.send('Det ringer på! Kan *noen* åpne, please!? :bell:', function(err, res) {
       if (err) {
           console.log('Error:', err)
       } else {
